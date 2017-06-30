33827935a3f60a53fdf18cdd72ec97ead6504e08
shin-migami <davsuvo@gmail.com>
2017-06-30 21:48:50 +0530

    Added Image labeler script file

---
+++ labelIm.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from glob import glob
+
+#create blank array size equal 257 columns
+countSet = np.array([], dtype=np.int16).reshape(0,257)
+
+#get names of all image file in Frames folder
+imageFiles = glob('Frames/*.png')
+
+#iterate for each image
+for images in imageFiles:
+
+    #read image as grayscale
+    imArr = cv2.imread(images,cv2.COLOR_BGR2GRAY)
+
+    #count unique pixel
+    #NOTE: some color maynot exist
+    unique,colCounts = np.unique(imArr, return_counts=True)
+    
+    #show histogram to user
+    plt.hist(imArr.ravel(),256,[0,256])
+    plt.show(block=False)
+
+    #pack unique and count values together
+    uniqueDict = dict(zip(unique,colCounts))
+    counts = np.arange(256)
+    #make sure the count size is 256
+    for pixVal in uniqueDict.keys():
+        counts[pixVal] = uniqueDict[pixVal]
+
+    #ask for label
+    label = int(raw_input('Enter Label 1 or 2'))
+    
+    #append the label
+    counts = np.append(counts,[label])
+    
+    #add it to dataset
+    countSet = np.vstack((countSet, counts))
+
+#save as dataset
+np.savetxt('dataset.csv', countSet.astype(int),fmt='%i', delimiter=',')
