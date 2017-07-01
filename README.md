1fe93a1fd1ea436fda3957a8b2d1429ac91dd042
shin-migami <davsuvo@gmail.com>
2017-07-01 14:58:30 +0530

    Added folderwise automation

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ Improves throughput of data-set labelleing
 
 
 ### Usage  
- 1. Put image files in `Frames` folder in the same directory  
+ 1. Put Gaussian Noise image files in `Gauss` and others in `NonGauss` folder in the same directory  
  2. Initiate Script  
- 3. Label by looking at the histogram plot  
- 4. Enjot your dataset as CSV file  
+ 3. Enjot your dataset as CSV file  
--- labelIm.py
+++ labelIm.py
@@ -6,38 +6,37 @@ from glob import glob
 #create blank array size equal 257 columns
 countSet = np.array([], dtype=np.int16).reshape(0,257)
 
-#get names of all image file in Frames folder
-imageFiles = glob('Frames/*.png')
+#set folder names and corresponding labels
+folders = ['Gauss', 'NonGauss']
+label   = [1,2]
 
-#iterate for each image
-for images in imageFiles:
+for i,folder in enumerate(folders):
 
-    #read image as grayscale
-    imArr = cv2.imread(images,cv2.COLOR_BGR2GRAY)
+    #get names of all image file in Frames folder
+    imageFiles = glob(folder+'/*')
 
-    #count unique pixel
-    #NOTE: some color maynot exist
-    unique,colCounts = np.unique(imArr, return_counts=True)
+    #iterate for each image
+    for images in imageFiles:
+
+        #read image as grayscale
+        imArr = cv2.imread(images,cv2.COLOR_BGR2GRAY)
+
+        #count unique pixel
+        #NOTE: some color maynot exist
+        unique,colCounts = np.unique(imArr, return_counts=True)
+
+        #pack unique and count values together
+        uniqueDict = dict(zip(unique,colCounts))
+        counts = np.arange(256)
+        #make sure the count size is 256
+        for pixVal in uniqueDict.keys():
+            counts[pixVal] = uniqueDict[pixVal]
     
-    #show histogram to user
-    plt.hist(imArr.ravel(),256,[0,256])
-    plt.show(block=False)
-
-    #pack unique and count values together
-    uniqueDict = dict(zip(unique,colCounts))
-    counts = np.arange(256)
-    #make sure the count size is 256
-    for pixVal in uniqueDict.keys():
-        counts[pixVal] = uniqueDict[pixVal]
-
-    #ask for label
-    label = int(raw_input('Enter Label 1 or 2'))
+        #append the label
+        counts = np.append(counts,[label[i]])
     
-    #append the label
-    counts = np.append(counts,[label])
-    
-    #add it to dataset
-    countSet = np.vstack((countSet, counts))
+        #add it to dataset
+        countSet = np.vstack((countSet, counts))
 
 #save as dataset
 np.savetxt('dataset.csv', countSet.astype(int),fmt='%i', delimiter=',')
